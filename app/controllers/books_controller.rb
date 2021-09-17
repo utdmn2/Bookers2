a78cf605175d42177d06d635b1a34526e0cbbbf6
utdmn2 <utdmn2@gmail.com>
2021-09-17 05:21:54 +0000

    [Fix]Booksルーティング

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,31 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+  
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  private
+
+  def book_params
+    paramas.require(:book).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: 'homes#top'
+  
+  resources :books
+
   resources :users, only: [:show, :edit, :update]
 end
 
