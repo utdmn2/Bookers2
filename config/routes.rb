f7ab16811272be3f0fa35a02e110574663213b42
utdmn2 <utdmn2@gmail.com>
2021-09-17 06:05:04 +0000

    [Add]About画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  
+
+  get 'about' => 'homes#about'
+
   resources :books
 
   resources :users, only: [:show, :edit, :update]
