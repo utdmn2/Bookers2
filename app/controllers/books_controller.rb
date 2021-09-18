fcef447847e26bc93f1a7a1f49032cbc1d0ff92e
utdmn2 <utdmn2@gmail.com>
2021-09-18 08:40:21 +0000

    [Add]Update機能、新規投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,15 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
-    
+    redirect_to book_path(@book)
+
   end
-  
+
   def index
     @books = Book.all
   end
@@ -18,20 +18,28 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def edit
-  
+    @book = Book.find(params[:id])
   end
-  
+
   def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+    redirect_to book_path(@book.id) ,notice: "You have updated book successfully."
+    else
+    render :show
+    end
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
+    book = Book.find(params[:id]) #データ(レコード)を一件取得
+    book.destroy  #データ（レコード）を削除
+    redirect_to books_path 
+
   end
-  
+
   private
 
 
