e7d881fd5f047de61e2ceb80a282a5ed17127089
utdmn2 <utdmn2@gmail.com>
2021-09-30 03:10:42 +0000

    [Start]応用課題５

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_113715) do
+ActiveRecord::Schema.define(version: 2021_09_30_030026) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 2021_09_28_113715) do
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
