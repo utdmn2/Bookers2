14e9e7bda2708aab28dc394a67670f0e6aa0ad28
utdmn2 <utdmn2@gmail.com>
2021-10-03 09:33:18 +0000

    [Start]コメント機能非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,12 +2,14 @@ class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
+    # redirect_to request.referer
   end
 
   def destroy
     BookComment.find_by(id: params[:id]).destroy
+    # redirect_to request.referer
   end
 
      private
